269c3329023ab5d5736de7eb660c571d8d6828af
S.Pratap Yadav <87929992+iSPYadav01@users.noreply.github.com>
2024-12-31 16:26:34 +0530

    Add initial project setup and CI/CD pipeline

--- model/model.py
+++ model/model.py
@@ -1,12 +1,8 @@
 from sklearn.linear_model import LinearRegression
-import numpy as np
-
 class SimpleModel:
     def __init__(self):
         self.model = LinearRegression()
-
     def train(self, X, y):
         self.model.fit(X, y)
-
     def predict(self, X):
         return self.model.predict(X)
